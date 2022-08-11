83dd643138ff292aa79742b8d35f2c45f9695cfd
Jamie Chau <j.chau905@gmail.com>
2022-08-11 14:29:01 -0400

    completed lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,35 @@
 class ListsController < ApplicationController
+  before_action :set_lists, only: %i[ show ]
+
+  def index
+    @lists = List.all
+
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_lists)
+    if @list.save
+      redirect_to list_path(@list), notice: "Restaurant was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_lists
+    @list = List.find(params[:id])
+  end
+
+  def params_lists
+    params.require(:list).permit(:name)
+  end
+
 end
