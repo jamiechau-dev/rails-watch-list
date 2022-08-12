2cda3cc69aa5e22c67b82607b1b25ce3bf2ae858
Jamie Chau <j.chau905@gmail.com>
2022-08-12 16:45:57 -0400

    added the delete list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -18,6 +18,7 @@ class BookmarksController < ApplicationController
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
+    redirect_to lists_path, notice: "Restaurant was successfully destroyed."
   end
 
   private
